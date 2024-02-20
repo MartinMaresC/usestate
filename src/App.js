c6bd33b3ca5e09bf8f2a2adfbe5c5e354de7dc54
Martin Mares <78748150+MartinMaresC@users.noreply.github.com>
2024-02-19 19:54:59 -0600

    App.js changed
    Simple usState used to modify a name back and forth on button click

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+const [name, setName] = useState("Martin");
+const nameChange = () =>{
+  setName((prevName)=> (prevName === "Martin" ? "Arturo" : "Martin"));
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{name}</p>
+      <button onClick={nameChange}>Click to change name</button>
     </div>
   );
 }
